7d0b8376ee9c4d85e8c1b9ad694d4b7a041aea43
Gregory John Casamento <greg.casamento@gmail.com>
2022-02-03 19:25:25 -0500

    Change to use older NSAutoreleasePool

--- NSNumberFormatter_test/main.m
+++ NSNumberFormatter_test/main.m
@@ -9,24 +9,26 @@
 #import <Foundation/NSException.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSNumberFormatter *nf = [NSNumberFormatter new];
-        NSString *expected = @"0.9";
-        NSString *got;
-        [nf setMaximumFractionDigits:3];
-        got = [nf stringFromNumber:[NSNumber numberWithDouble:0.9]];
-        // NSAssert([expected isEqualToString:got], @"number formatter botch, expected \"%@\", got \"%@\"", expected, got);
-        
-        if ([expected isEqualToString: got])
-        {
-            NSLog(@"Pass");
-        }
-        else
-        {
-            NSLog(@"Fail");
-        }
-        
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+  // insert code here...
+  NSNumberFormatter *nf = [NSNumberFormatter new];
+  NSString *expected = @"0.9";
+  NSString *got;
+  [nf setMaximumFractionDigits:3];
+  got = [nf stringFromNumber:[NSNumber numberWithDouble:0.9]];
+  // NSAssert([expected isEqualToString:got], @"number formatter botch, expected \"%@\", got \"%@\"", expected, got);
+  
+  if ([expected isEqualToString: got])
+    {
+      NSLog(@"Pass");
     }
-    return 0;
+  else
+    {
+      NSLog(@"Fail");
+    }
+  
+  RELEASE(pool);
+  
+  return 0;
 }
